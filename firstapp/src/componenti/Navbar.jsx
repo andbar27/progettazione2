519d50fbc2252e0eb3c002323582fc4e328a402f
andbar27 <andrea.barbato2796@icloud.com>
2024-11-21 13:01:27 +0100

    minor

---
+++ firstapp/src/componenti/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <ul>
+            <li><a href="#">Ciao</a></li>
+            <li><a href="#">Ciao</a></li>
+            <li><a href="#">Ciao</a></li>
+        </ul>
+    )
+}
+
+export default Navbar
