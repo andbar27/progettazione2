2bf3993a6675d314eb828efd8c7573b70118cb6b
andbar27 <andrea.barbato2796@icloud.com>
2024-12-10 11:01:13 +0100

    list card

--- 20241210react3/src/App.jsx
+++ 20241210react3/src/App.jsx
@@ -9,9 +9,56 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
+  const cities =[
+    {
+      id:0,
+      name: "Tokyo",
+      desc: "Capitale del Giappone",
+      imgUrl: "https://www.bandiere.it/uploads/2016-6-6/1200-0/tokyo.jpg",
+      isVisit: false,
+    },
+    {
+      id:1,
+      name: "Roma",
+      desc: "Capitale del Mondo",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/3/31/Roma-Stemma-2.svg",
+      isVisit: true,
+    },
+    {
+      id:2,
+      name: "New York",
+      desc: "Capitale di New York",
+      imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSP8xJELCTKJplZFoq9yG9JwO8zRto1jiHTg&s",
+      isVisit: false,
+    },
+  ]
+
   return (
     <>
       <div>
+          {cities.map((city) => (
+            <Card 
+              key = {city.id}
+              titolo={city.name}
+              imgUrl={city.imgUrl}
+              isVisit={city.isVisit}
+              descrizione={city.desc}
+            >
+            </Card>
+          ))}
+
+          {cities.filter((city) => city.isVisit == true).map(
+            (city)=>(
+              <Card 
+                key = {city.id}
+                titolo={city.name}
+                imgUrl={city.imgUrl}
+                isVisit={city.isVisit}
+                descrizione={city.desc}
+              >
+              </Card>
+            ))}
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -31,7 +78,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <Card></Card>
     </>
   )
 }
